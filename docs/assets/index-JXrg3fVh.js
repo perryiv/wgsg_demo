981e9b80f6afc98c105f550fb712c7a0529272e1
Perry Miller <perry@modelspace.com>
2026-05-01 02:08:56 -0400

    Updated demo

--- docs/assets/index-JXrg3fVh.js
+++ docs/assets/index-JXrg3fVh.js
@@ -20651,7 +20651,7 @@ function App() {
   );
   const buildTimeStamp = reactExports.useMemo(
     () => {
-      const date = /* @__PURE__ */ new Date(1777614268752);
+      const date = /* @__PURE__ */ new Date(1777615733360);
       const Y = date.getFullYear();
       const M = String(date.getMonth() + 1).padStart(2, "0");
       const D = String(date.getDate()).padStart(2, "0");
@@ -32798,4 +32798,4 @@ clientExports.createRoot(document.getElementById("root")).render(
     /* @__PURE__ */ jsxRuntimeExports.jsx(App, {})
   ] }) })
 );
-//# sourceMappingURL=index-CcXR4r_Q.js.map
+//# sourceMappingURL=index-JXrg3fVh.js.map
